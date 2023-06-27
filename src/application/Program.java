e4fa69fdd5860553d8063d03835aac089ad14bee
Matheus Klabunde <mt.klabunde.dev@gmail.com>
2023-06-27 15:10:58 -0400

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Department obj = new Department(1, "Books");
-		System.out.println("Deparment created: " + obj);
 
-		SellerDao selleDao = DaoFactory.createSellerDao();
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(1, "Matheus Klabunde", "matheus@gmail.com", new Date(), 2000.0, obj);
-		System.out.println("Seller created: " + seller);
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 }
